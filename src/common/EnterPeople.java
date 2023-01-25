65a3691e6d2ff9a30d562e9021eb769a1007b6c4
AliHejazi1382 <mrhjz08@gmail.com>
2023-01-25 20:47:27 +0330

    feat(User class), create EnterPeople class

---
+++ src/common/EnterPeople.java
@@ -0,0 +1,31 @@
+package common;
+
+import user.User;
+
+import java.util.Scanner;
+
+public class EnterPeople {//To get the information of User
+    //Like cin in cpp, get values in terminal
+    private Scanner scanner;
+    private User user;
+
+    public EnterPeople() {
+        scanner = new Scanner(System.in);
+        user = new User();
+    }
+
+    public User getUser() {// getting username and password
+        System.out.println("Enter your username:");
+        String username = scanner.next();
+        System.out.println("Enter your password:");
+        String password = scanner.next();
+
+        setUser(username, password);
+        return user;
+    }
+
+    public void setUser(String username, String password) {// set username and password for user
+        user.setUsername(username);
+        user.setPassword(password);
+    }
+}
